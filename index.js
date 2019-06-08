621e1840492b4326d3915121ac0ae65bf339ca1b
Dale Salter <dalejsalter@outlook.com>
2019-06-08 17:32:06 +1000

    Changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Hello Meme #4'));
+app.get('/', (req, res) => res.send('Hello Meme #5'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
